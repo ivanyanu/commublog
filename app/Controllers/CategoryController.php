9e71f7011e3212e38f62458c5a861ec8cb089cb7
Ivan Yanu <ivan.yanu@gmail.com>
2023-07-11 16:48:24 -0300

    Creé los equeletos de los controladores

---
+++ app/Controllers/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class CategoryController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
