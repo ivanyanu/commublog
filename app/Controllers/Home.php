283169fd3ddb5faf64011ef9977c746db24dcda5
Ivan Yanu <ivan.yanu@gmail.com>
2023-07-13 17:16:15 -0300

    Envío de email básico

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,26 @@ class Home extends BaseController
 {
     public function index()
     {
+        $email = \Config\Services::email();
+
+        $config = array(
+            'protocol' => 'smtp',
+            'SMTPHost' => 'sandbox.smtp.mailtrap.io',
+            'SMTPUser' => '024702e0c51c59',
+            'SMTPPass' => 'ebf49f49297802',
+            'SMTPPort' => 2525,
+            'wordWrap' => true,
+        );
+
+        $email->initialize($config);
+
+        $email->setFrom('your@example.com', 'Your Name');
+        $email->setTo('ivan.yanu@gmail.com');
+        $email->setSubject('Email Test');
+        $email->setMessage('Testing the email class.');
+
+        $send = $email->send();
+        return 'ivan.yanu@gmail.com'.print_r($send);
         return view('welcome_message');
     }
 }
